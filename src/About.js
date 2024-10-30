193cd64a78ab46472a3049e56c8c01dedf735784
shubhkaran30546 <73439286+shubhkaran30546@users.noreply.github.com>
2024-10-29 23:36:48 -0600

    commit

--- src/About.js
+++ src/About.js
@@ -6,12 +6,9 @@ export default function About() {
   return (
     <body>
       <h2 className='aboutheader'>About Me!</h2>
-        <p className="summary">I am a 4th year student studying in Computing Science at the University of Alberta. I am passionate about computer networks and security. With a strong focus on these domains, I am dedicated to expanding my knowledge and expertise in this ever-evolving field.
-I am currently preparing to take the Cisco Certified Network Associate (CCNA) and cybersecurity exams to earn these valuable certifications. 
-These certifications will not only validate my skills but also help me stay updated with the latest technologies and best practices in networking and security.
-In addition to my focus on networking and security, I am also gaining knowledge in cloud computing.
-I recently earned the AWS Cloud Practitioner certification, which has provided me with a solid foundation in cloud services and solutions. This certification demonstrates my understanding of AWS core services and my ability to implement cloud-based solutions effectively.
-With a commitment to continuous learning and professional growth, I am excited about the opportunities in the field of computer networks and security. I look forward to contributing to innovative projects and collaborating with like-minded professionals to create secure and efficient network infrastructures.</p>
+        <p className="summary">I am a 4th-year Computing Science student at the University of Alberta with a keen interest in software development and a passion for building scalable, efficient, and user-centered applications. My academic journey has provided me with a solid foundation in programming, system design, and cloud computing, with recent hands-on experience in cloud services through the AWS Cloud Practitioner certification. This certification has equipped me with the skills to design and implement cloud-based solutions effectively, enhancing my software development abilities.
+
+While software development is my primary focus, I am also dedicated to expanding my understanding of computer networks and security. Currently, I am preparing for the Cisco Certified Network Associate (CCNA) and cybersecurity exams to strengthen my grasp on secure network infrastructures. With a commitment to continuous learning, I look forward to contributing to innovative projects, leveraging both my software development skills and knowledge in security best practices to create reliable and secure applications that drive impactful results.</p>
       <section className = "about1">
       <h3 className='aboutheader1'>Language & Technologies</h3>
       <p className = "about2">Python Java XML C/C++ HTML CSS  Javascript SQL NoSQL R PHP C#</p>
